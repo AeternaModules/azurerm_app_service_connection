0f57af90cce2a8f161b8173234fb5022dee0d9a9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "app_service_connections" {
-  description = "All app_service_connection resources"
-  value       = azurerm_app_service_connection.app_service_connections
-  sensitive   = true
-}
 output "app_service_connections_app_service_id" {
-  description = "List of app_service_id values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.app_service_id]
+  description = "Map of app_service_id values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.app_service_id }
 }
 output "app_service_connections_authentication" {
-  description = "List of authentication values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.authentication]
+  description = "Map of authentication values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.authentication }
   sensitive   = true
 }
 output "app_service_connections_client_type" {
-  description = "List of client_type values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.client_type]
+  description = "Map of client_type values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.client_type }
 }
 output "app_service_connections_name" {
-  description = "List of name values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.name]
+  description = "Map of name values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.name }
 }
 output "app_service_connections_secret_store" {
-  description = "List of secret_store values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.secret_store]
+  description = "Map of secret_store values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.secret_store }
 }
 output "app_service_connections_target_resource_id" {
-  description = "List of target_resource_id values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.target_resource_id]
+  description = "Map of target_resource_id values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.target_resource_id }
 }
 output "app_service_connections_vnet_solution" {
-  description = "List of vnet_solution values across all app_service_connections"
-  value       = [for k, v in azurerm_app_service_connection.app_service_connections : v.vnet_solution]
+  description = "Map of vnet_solution values across all app_service_connections, keyed the same as var.app_service_connections"
+  value       = { for k, v in azurerm_app_service_connection.app_service_connections : k => v.vnet_solution }
 }
 
